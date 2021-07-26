30be78f62888307dbdab8c2335675df86791928c
Yuvaneswaran Dhanasekaran <yuvaneswaran.dha@tigeranalytics.local>
2021-07-26 15:20:51 +0530

    UI Code Updated

--- src/app/components/organisms/charts/calendar-simulated-chart/calendar-simulated-chart.component.ts
+++ src/app/components/organisms/charts/calendar-simulated-chart/calendar-simulated-chart.component.ts
@@ -232,18 +232,22 @@ export class CalendarSimulatedChartComponent implements OnInit {
         this.svg
             .append('text')
             .attr('class', 'label')
+            .attr('id', 'leftAxisLabel')
             .attr('x', -50)
             .attr('y', -25)
             .attr('text-anchor', 'start')
             .text('Discount depth');
-        // Discount Depth axis label
+
+        // ROI axis label
         this.svg
             .append('text')
             .attr('class', 'label')
-            .attr('x', this.boundingWidth)
+            .attr('id', 'rightAxisLabel')
+            .attr('x', this.boundingWidth - 45) // Programatically change this value based on the selection for Seasonality Index
+            // .attr('x', this.boundingWidth) // Programatically change this value based on the selection for ROI
             .attr('y', -25)
             .attr('text-anchor', 'start')
-            .text(label);
+            .text('Seasonality Index'); // Programatically change this value based on the selection for Seasonality Index
 
         const chart = this.svg;
 
