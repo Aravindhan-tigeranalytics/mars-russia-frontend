5bc5a522b4006bb66a4e1700b6d2ef9e6deb97cd
Yuvaneswaran Dhanasekaran <yuvaneswaran.dha@tigeranalytics.local>
2021-07-29 09:59:11 +0530

    Added Loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,10 +12,38 @@ import { PagesModule } from '@pages/pages.module';
 import {CoreModule} from "./core/core.module"
 
 import {SharedModule} from "./shared/shared.module"
+import {
+    NgxUiLoaderModule,
+    NgxUiLoaderHttpModule,
+    NgxUiLoaderConfig,
+    SPINNER,
+    POSITION,
+    PB_DIRECTION,
+  } from "ngx-ui-loader";
+
+  const ngxUiLoaderConfig: NgxUiLoaderConfig = {
+    fgsColor: '#F4F4F5',
+    logoUrl: "assets/favicons/loader-logo.jpg",
+    logoPosition: "center-center",
+    logoSize: 90,
+    fgsPosition: POSITION.centerCenter,
+    pbDirection: PB_DIRECTION.leftToRight,
+    fgsSize: 40,
+    fgsType: SPINNER.squareJellyBox,
+    pbThickness: 5
+  };
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, ComponentModule, PagesModule,CoreModule , SharedModule],
+    imports: [
+        BrowserModule, 
+        AppRoutingModule, 
+        ComponentModule, 
+        PagesModule,CoreModule, 
+        SharedModule,
+        NgxUiLoaderModule.forRoot(ngxUiLoaderConfig),
+        NgxUiLoaderHttpModule.forRoot({ showForeground: true })
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
