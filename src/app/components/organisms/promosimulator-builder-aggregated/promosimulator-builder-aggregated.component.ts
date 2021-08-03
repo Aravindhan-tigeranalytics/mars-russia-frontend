cac4da56f185fb151109ee4918b4f650da4db5ac
Yuvaneswaran Dhanasekaran <yuvaneswaran.dha@tigeranalytics.local>
2021-08-03 18:46:15 +0530

    Horizontal scroll fixed in graph

--- src/app/components/organisms/promosimulator-builder-aggregated/promosimulator-builder-aggregated.component.ts
+++ src/app/components/organisms/promosimulator-builder-aggregated/promosimulator-builder-aggregated.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angula
 import { SimulatorService } from "../../../core/services/simulator.service";
 import { OptimizerService } from "../../../core/services/optimizer.service";
 import * as Utils from "../../../core/utils/util"
+import * as $ from 'jquery';
 
 @Component({
     selector: 'nwn-promosimulator-builder-aggregated',
@@ -9,6 +10,8 @@ import * as Utils from "../../../core/utils/util"
     styleUrls: ['./promosimulator-builder-aggregated.component.css'],
 })
 export class PromosimulatorBuilderAggregatedComponent implements OnInit, AfterViewInit {
+    @ViewChild('scrollOne') scrollOne: ElementRef;
+    @ViewChild('scrollTwo') scrollTwo: ElementRef;
     translate_y: string = '';
     currentTranslateRate: string = '';
     constructor(private elRef: ElementRef,public restApi: SimulatorService,public optimize:OptimizerService) {}
@@ -44,7 +47,27 @@ export class PromosimulatorBuilderAggregatedComponent implements OnInit, AfterVi
     activeWeeklyTab: string = 'absolute'
     activeAggregatedTab: string = 'absolute'
 
+    updateScroll(value:any){
+        if(value == 'Graph'){
+            const scrollOne = this.scrollOne.nativeElement as HTMLElement;
+            const scrollTwo = this.scrollTwo.nativeElement as HTMLElement;
+
+            scrollTwo.scrollLeft = scrollOne.scrollLeft;
+        }
+        else if(value == 'Table'){
+            const scrollOne = this.scrollOne.nativeElement as HTMLElement;
+            const scrollTwo = this.scrollTwo.nativeElement as HTMLElement;
+
+            scrollOne.scrollLeft = scrollTwo.scrollLeft;
+        }
+      }
+
     ngOnInit() {
+        // $(document).ready(function() {
+        //     $('#div2').on('scroll', function () {
+        //       $('#div1').scrollLeft($(this).scrollLeft());
+        //     });
+        // });
         // this.restApi.uploadedSimulatorDataObservable.asObservable().subscribe(data=>{
         //     if(data != ''){
         //         console.log(data,"observable data")
