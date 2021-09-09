e6fad3d23274a0e6380c4ee20ed7c0731b13cc8a
Yuvaneswaran Dhanasekaran <yuvaneswaran.dha@tigeranalytics.local>
2021-09-09 12:59:23 +0530

    product images

--- src/app/shared/pipe/image.pipe.ts
+++ src/app/shared/pipe/image.pipe.ts
@@ -5,23 +5,37 @@ export class ImagePipe implements PipeTransform {
    
   transform(product:any): any {
       let ret = "/assets/subbrand-img/orbit-otc.png"
-    console.log(product , "product in image pipe")
-    // debugger;
+      console.log(product , "product in image pipe")
      
     switch (product) {
-        
         case "Big Bars":
-          ret =  "/assets/subbrand-img/bigbars.jpg"
+          // ret =  "/assets/subbrand-img/bigbars.jpg"
+          ret =  "/assets/subbrand-img/bars.jpg"
           break
         case "Orbit OTC":
           ret =  "/assets/subbrand-img/orbit-otc.png"
           break
-        case "A.Korkunov 192g":
-            ret =  "/assets/subbrand-img/A-Korkunov.jpg"
-            break
+        case "A.Korkunov 192g" || "A.Korkunov 256g" || "A.Korkunov 110g":
+          ret =  "/assets/subbrand-img/A-Korkunov.jpg"
+          break
+        case "Skittles 38g":
+          ret =  "/assets/subbrand-img/skittles.jpg"
+          break
+        case "Orbit XXL":
+          ret =  "/assets/subbrand-img/orbit-xxl.jpg"
+          break
+        case "M&M 360g":
+          ret =  "/assets/subbrand-img/m&m360.jpg"
+          break
+        case "M&M 45g":
+          ret =  "/assets/subbrand-img/m&m45.jpg"
+          break
+        case "Snickers Crisp Trio":
+          ret =  "/assets/subbrand-img/Crisp-Trio.jpg"
+          break
         default:
             break 
       }
       return ret
-} 
+  } 
 }
